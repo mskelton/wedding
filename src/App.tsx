254a783a8f862146defa1861bf1304d8f5ebeb95
Mark Skelton <mdskelton99@gmail.com>
2024-11-12 20:45:39 -0600

    Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import { FAQs } from "./components/FAQs"
 import { Footer } from "./components/Footer"
 import { Hero } from "./components/Hero"
 import { OurStory } from "./components/OurStory"
-import { useThemeToggle } from "./components/ThemeToggle"
 import { Gallery } from "./Gallery"
 import { Header } from "./Header"
 import { RSVP } from "./RSVP"
 
 export default function Home() {
-  useThemeToggle()
-
   return (
     <div style={{ "--header-height": "4rem" } as React.CSSProperties}>
       <div id="home" />
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,4 @@
 export const dates = {
-  reception: new Date("2025-02-15T17:00:00+05:00"),
-  wedding: new Date("2025-02-15T15:00:00+05:00"),
+  reception: new Date("2025-02-15T15:00:00-06:00"),
+  wedding: new Date("2025-02-15T14:00:00-06:00"),
 }
