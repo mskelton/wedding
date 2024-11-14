a1051269b1c8a51a862dea59382ae6d65523d6da
Mark Skelton <mdskelton99@gmail.com>
2024-11-14 17:33:38 -0600

    Quotes

--- eslint-local-rules/no-unescaped-quotes.js
+++ eslint-local-rules/no-unescaped-quotes.js
@@ -1,78 +1,66 @@
-const entities = [
-  { alternatives: ["“", "”"], char: '"' },
-  { alternatives: ["’", "‘"], char: "'" },
-]
-
-/**
- * @param {object} node - Node to check.
- * @returns {boolean} Whether or not the node if a JSX element or fragment.
- */
 function isJSX(node) {
-  return node && ["JSXElement", "JSXFragment"].indexOf(node.type) >= 0
+  return node && (node.type === "JSXElement" || node.type === "JSXFragment")
 }
 
-/**
- * @param {import("eslint").RuleContext} context
- * @param {Literal | JSXText} node
- */
-function reportUnescaped(context, node) {
-  const source = context.getSourceCode()
-
-  // HTML entities are already escaped in node.value (as well as node.raw),
-  // so pull the raw text from context.getSourceCode()
-  for (let i = node.loc.start.line; i <= node.loc.end.line; i++) {
-    let rawLine = context.getSourceCode().lines[i - 1]
-    const start = i === node.loc.start.line ? node.loc.start.column : 0
-    const end = i === node.loc.end.line ? node.loc.end.column : rawLine.length
-
-    rawLine = rawLine.slice(start, end)
-
-    for (const entity of entities) {
-      for (let index = 0; index < rawLine.length; index++) {
-        if (rawLine[index] === entity.char) {
-          context.report({
-            data: { entity: entity.char },
-            loc: { column: start + index, line: i },
-            messageId: "unescapedEntity",
-            node,
-            suggest: entity.alternatives.map((alt) => ({
-              data: {
-                alt,
-                entity: entity.char,
-              },
-              fix: (fixer) => {
-                const rangeStart = source.getIndexFromLoc({
-                  column: start + index,
-                  line: i,
-                })
-
-                return fixer.replaceTextRange([rangeStart, rangeStart + 1], alt)
-              },
-              messageId: "unescapedEntitySuggestion",
-            })),
-          })
-        }
-      }
-    }
-  }
-}
-
+/** @type {import("eslint").Rule.RuleModule} */
 module.exports = {
   create(context) {
+    function replace({ from, loc, to }) {
+      context.report({
+        data: { entity: from },
+        fix: (fixer) => {
+          const rangeStart = context.sourceCode.getIndexFromLoc(loc)
+          return fixer.replaceTextRange([rangeStart, rangeStart + 1], to)
+        },
+        loc,
+        messageId: "unescapedEntity",
+      })
+    }
+
     return {
       "Literal, JSXText"(node) {
-        if (isJSX(node.parent)) {
-          reportUnescaped(context, node)
+        if (!isJSX(node.parent)) {
+          return
+        }
+
+        // HTML entities are already escaped in node.value (as well as node.raw),
+        // so pull the raw text from context.getSourceCode()
+        for (let i = node.loc.start.line; i <= node.loc.end.line; i++) {
+          let rawLine = context.sourceCode.lines[i - 1]
+          const start = i === node.loc.start.line ? node.loc.start.column : 0
+          const end =
+            i === node.loc.end.line ? node.loc.end.column : rawLine.length
+
+          rawLine = rawLine.slice(start, end)
+
+          let isOpen = false
+
+          for (let index = 0; index < rawLine.length; index++) {
+            if (rawLine[index] === "'") {
+              replace({
+                from: "'",
+                loc: { column: start + index, line: i },
+                to: "’",
+              })
+            } else if (rawLine[index] === '"') {
+              replace({
+                from: '"',
+                loc: { column: start + index, line: i },
+                to: isOpen ? "”" : "“",
+              })
+
+              isOpen = !isOpen
+            }
+          }
         }
       },
     }
   },
   meta: {
     fixable: "code",
-    hasSuggestions: true,
+    hasSuggestions: false,
     messages: {
       unescapedEntity: "HTML entity, {{entity}}, must be escaped.",
-      unescapedEntitySuggestion: "Replace {{entity}} with {{alt}}.",
     },
     schema: [],
     type: "suggestion",
